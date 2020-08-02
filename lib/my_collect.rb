7bb14f89ca35b6841bca237c9ea1d0555e417197
Valentina E <tiyahelfrih@gmail.com>
2020-08-02 21:20:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+describe "my_collect" do
+  let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
+  let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
 
+  it "can handle an empty collection" do
+    empty_array = []
+    counter = 0
+      my_collect(empty_array) do |x|
+        counter += 1
+      end
+    expect(counter).to eq(0)
+  end
 
